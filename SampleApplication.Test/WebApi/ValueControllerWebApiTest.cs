0041e16011a9d269c534744501255d2e98f5a63e
Ankt <ankyyyjst4u@live.com>
2018-07-05 07:10:40 +0000

    My first commit

---
+++ SampleApplication.Test/WebApi/ValueControllerWebApiTest.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="ValueControllerWebApiTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SampleApplication.Test.WebApi
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Moq;
+    using SampleApplication.Controllers;
+    using Xunit;
+
+    public class ValueControllerWebApiTest
+    {
+        private readonly ValuesController valueService;
+
+        public ValueControllerWebApiTest()
+        {
+            this.valueService = new ValuesController();
+        }
+
+        [Fact]
+        public void ReturnGetDataById_Equal()
+        {
+            var result = this.valueService.Get(1);
+
+            Assert.Equal("value", result);
+        }
+
+        [Fact]
+        public void ReturnGetDataById_NotEqual()
+        {
+            var result = this.valueService.Get(1);
+
+            Assert.NotEqual("values will not match", result);
+        }
+
+        [Fact]
+        public void ConfigureServices_RegistersDependenciesCorrectly()
+        {
+            //  Arrange
+
+            //  Setting up the stuff required for Configuration.GetConnectionString("DefaultConnection")
+            Mock<IConfigurationSection> configurationSectionStub = new Mock<IConfigurationSection>();
+
+            Mock<Microsoft.Extensions.Configuration.IConfiguration> configurationStub = new Mock<Microsoft.Extensions.Configuration.IConfiguration>();
+            //configurationStub.Setup(x => x.GetSection("ConnectionStrings")).Returns(configurationSectionStub.Object);
+
+            IServiceCollection services = new ServiceCollection();
+            var target = new Startup(configurationStub.Object);
+
+            //  Act
+
+            target.ConfigureServices(services);
+            //  Mimic internal asp.net core logic.
+            services.AddTransient<ValuesController>();
+
+            //  Assert
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var controller = serviceProvider.GetService<ValuesController>();
+            Assert.NotNull(controller);
+        }
+    }
+}
---
+++ SampleApplication/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="ValuesController.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SampleApplication.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+
+    /// <summary> Values Controller </summary>
+    /// <returns>returns</returns>
+    public class ValuesController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        /// <summary>Get All values based on ID</summary>
+        /// <returns>returns a value</returns>
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
